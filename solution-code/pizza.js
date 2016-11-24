f13b621331f4b00b5ef35456d54c19301afb726a
LluisArevalo <thorin119@gmail.com>
2016-11-24 09:10:17 +0100

    Iteration 3

--- solution-code/pizza.js
+++ solution-code/pizza.js
@@ -1,23 +1,35 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function() {
-  $(".crust").removeClass("crust-gluten-free");
-  $(".crust").find(".sauce").removeClass("sauce-white");
+  defaultValues();
 });
-
-function btnHandler(triggerClass, targetClass, toggleClass) {
-  if (toggleClass) {
-    $(triggerClass).on("click", function() {
-      $(targetClass).toggleClass(toggleClass);
-    });
-  } else {
-    $(triggerClass).on("click", function() {
-      $(targetClass).toggle();
-    });
-  }
-}
 
 btnHandler(".btn-pepperonni", ".pep");
 btnHandler(".btn-mushrooms", ".mushroom");
 btnHandler(".btn-green-peppers", ".green-pepper");
 btnHandler(".btn-crust", ".crust", "crust-gluten-free");
 btnHandler(".btn-sauce", ".sauce", "sauce-white");
+
+function defaultValues () {
+  $(".crust").removeClass("crust-gluten-free");
+  $(".crust").find(".sauce").removeClass("sauce-white");
+  $(".btn-pepperonni, .btn-mushrooms, .btn-green-peppers").addClass("active");
+  $(".btn-crust, .btn-sauce").removeClass("active");
+}
+
+function btnHandler(triggerClass, targetClass, toggleClass) {
+  if (toggleClass) {
+    $(triggerClass).on("click", function() {
+      $(targetClass).toggleClass(toggleClass);
+      changeButtonStyle($(this));
+    });
+  } else {
+    $(triggerClass).on("click", function() {
+      $(targetClass).toggle();
+      changeButtonStyle($(this));
+    });
+  }
+}
+
+function changeButtonStyle(button) {
+  $(button).toggleClass("active");
+}
