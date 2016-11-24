28b95f05ae68932a700fa3f3945c22afa1b0d187
LluisArevalo <thorin119@gmail.com>
2016-11-24 09:02:52 +0100

    Iterations 1 and 2

--- solution-code/pizza.js
+++ solution-code/pizza.js
@@ -1,1 +1,23 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+  $(".crust").removeClass("crust-gluten-free");
+  $(".crust").find(".sauce").removeClass("sauce-white");
+});
+
+function btnHandler(triggerClass, targetClass, toggleClass) {
+  if (toggleClass) {
+    $(triggerClass).on("click", function() {
+      $(targetClass).toggleClass(toggleClass);
+    });
+  } else {
+    $(triggerClass).on("click", function() {
+      $(targetClass).toggle();
+    });
+  }
+}
+
+btnHandler(".btn-pepperonni", ".pep");
+btnHandler(".btn-mushrooms", ".mushroom");
+btnHandler(".btn-green-peppers", ".green-pepper");
+btnHandler(".btn-crust", ".crust", "crust-gluten-free");
+btnHandler(".btn-sauce", ".sauce", "sauce-white");
