921af6dc3116be48263533853f42cbef6cbeede6
LluisArevalo <thorin119@gmail.com>
2016-11-24 11:10:30 +0100

    Change iteration 4 and do the solution

--- solution-code/pizza.js
+++ solution-code/pizza.js
@@ -3,33 +3,64 @@ $(document).ready(function() {
   defaultValues();
 });
 
-btnHandler(".btn-pepperonni", ".pep");
+var prices = {
+  "peppperonni": 1,
+  "mushroom": 1,
+  "green-pepper": 1,
+  "sauce": 3,
+  "crust": 5
+};
+
+btnHandler(".btn-pepperonni", ".pepperonni");
 btnHandler(".btn-mushrooms", ".mushroom");
 btnHandler(".btn-green-peppers", ".green-pepper");
 btnHandler(".btn-crust", ".crust", "crust-gluten-free");
 btnHandler(".btn-sauce", ".sauce", "sauce-white");
 
 function defaultValues () {
+  // Ingredients
   $(".crust").removeClass("crust-gluten-free");
   $(".crust").find(".sauce").removeClass("sauce-white");
+
+  // Buttons state
   $(".btn-pepperonni, .btn-mushrooms, .btn-green-peppers").addClass("active");
   $(".btn-crust, .btn-sauce").removeClass("active");
+
+  // Ingredients list
+  $(".panel.price").find("ul").find("li:contains('sauce')").toggle();
+  $(".panel.price").find("ul").find("li:contains('crust')").toggle();
 }
 
 function btnHandler(triggerClass, targetClass, toggleClass) {
-  if (toggleClass) {
-    $(triggerClass).on("click", function() {
-      $(targetClass).toggleClass(toggleClass);
-      changeButtonStyle($(this));
-    });
-  } else {
-    $(triggerClass).on("click", function() {
-      $(targetClass).toggle();
-      changeButtonStyle($(this));
-    });
-  }
+  $(triggerClass).on("click", function() {
+    toggleClass ? $(targetClass).toggleClass(toggleClass) : $(targetClass).toggle();
+    changeButtonStyle($(this));
+    updateIngredientsList(targetClass);
+    // updatePrice($(this), targetClass);
+  });
 }
 
 function changeButtonStyle(button) {
   $(button).toggleClass("active");
 }
+
+function updateIngredientsList(ingredient) {
+  ingredient = ingredient.replace(".", "");
+  var element = $(".panel.price").find("ul").find("li:contains('" + ingredient + "')");
+  element.toggle();
+}
+
+// function updatePrice(button, ingredient) {
+//   ingredient = ingredient.replace(".", "");
+//   price      = prices[ingredient];
+//
+//   if ($(button).hasClass("active")) {
+//     price *= -1;
+//   }
+//
+//   updateTotal(price);
+// }
+//
+// function updateTotal (price) {
+//
+// }
